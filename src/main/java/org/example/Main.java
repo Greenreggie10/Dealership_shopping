1c5a54e8cb7df73578e22f7bff9541414d492ebb
Reginald Green <reggie.green10@yahoo.com>
2023-01-04 17:50:25 -0500

    "carType"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner();
 
+        System.out.println("Welcome to the java dealership!");
+
+        System.out.println(".select option 'a' to buy a car");
+        System.out.println(".select option 'b' to sell a car");
     }
 }
