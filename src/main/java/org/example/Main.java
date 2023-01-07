d5f4f7acfb06c5a812cc76bb36e2746b68361b94
Reginald Green <reggie.green10@yahoo.com>
2023-01-06 20:12:04 -0500

    "creditWay"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,12 +17,33 @@ public class Main {
             case "a":
                 System.out.println("What is your budget?");
                 int budget =scan.nextInt();
+                scan.nextLine();
                 if (budget >= 40000) {
-                    System.out.println("We have a Lexus 450 with your name on it!!!");
+                    System.out.println("\nWe have a Lexus 450 with your name on it!!!" +
+                            "\nDo you have any insurance? Please write 'yes' or 'no'");
+                    String insure = scan.nextLine();
+                    System.out.println("\nDo you have a license? Please write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    System.out.println("What is your credit score?");
+                    int creditLine = scan.nextInt();
+                    scan.nextLine();
+                    if(insure.equals("yes") && license.equals("yes") && creditLine >= 620){
+
+                    }
+
                 } else if (budget >= 10000) {
-                    System.out.println("Great! A Nissan Altima is available.");
+                    System.out.println("\nGreat! A Nissan Altima is available." +
+                            "\nDo you have any insurance? Please write 'yes' or 'no'");
+                    String insure = scan.nextLine();
+                    System.out.println("\nDo you have a license? Please write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    System.out.println("What is your credit score?");
+                    int creditLine = scan.nextInt();
+                    scan.nextLine();
+
+
                 }else {
-                    System.out.println("We don't sell cars under $10,000. Apologies!");
+                    System.out.println("\nWe don't sell cars under $10,000. Apologies!");
                 }
                 break;
 
